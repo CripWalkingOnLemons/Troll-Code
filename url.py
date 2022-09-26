dac9c5ae62e9b273732a0476586ca17aee9b55ef
CripWalkingOnLemons <113372967+CripWalkingOnLemons@users.noreply.github.com>
2022-09-26 12:25:16 -0400

    Add files via upload

---
+++ url.py
@@ -0,0 +1,34 @@
+from re import X
+import webbrowser
+import time
+import os
+import sys
+import wmi
+
+list=["twitch.tv/borjyyy"]
+
+br=["chrome.exe","msedge.exe","firefox.exe"]
+
+def browserloop():
+    x=1
+    while x <= 3:
+        x+=1
+        for i in list:
+            time.sleep(1)
+            webbrowser.open(i)
+    f = wmi.WMI()
+  
+    flag = 0
+  
+    # Iterating through all the running processes
+    for process in f.Win32_Process():
+        if br == process.Name:
+            print("Application is Running")
+            flag = 1
+            break
+  
+        if flag == 0:
+            browserloop()
+browserloop()
+
+
